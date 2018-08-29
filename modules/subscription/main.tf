863fad033c3d942540599e068261c538dac57ca5
Sascha <sascha@ziesig.net>
2018-08-29 15:40:43 +0200

    Hotfix: removed push subscriptions

--- modules/subscription/main.tf
+++ modules/subscription/main.tf
@@ -12,15 +12,4 @@ resource "google_pubsub_subscription" "pubsub_subscription_pull" {
   ack_deadline_seconds = "${lookup(var.pullSubscriptions[count.index], "deadline", 10)}"
 }
 
-resource "google_pubsub_subscription" "pubsub_subscription_push" {
-  count   = "${local.subscriptionPushLength}"
-  topic   = "${var.topicName}"
-  project = "${var.project}"
-
-  name                 = "${lookup(var.pushSubscriptions[count.index], "name")}"
-  ack_deadline_seconds = "${lookup(var.pushSubscriptions[count.index], "deadline", 10)}"
-
-  push_config {
-    push_endpoint = "${lookup(var.pushSubscriptions[count.index], "url")}"
-  }
 }
